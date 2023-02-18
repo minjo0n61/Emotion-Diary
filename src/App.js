b055b20f857c2dfa496ff991e734c40de17aea83
MinJoon_Choi <minjoon1998@gmail.com>
2023-02-19 00:41:57 +0900

    BrowserRoute-test1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+
+import RouteTest from './components/RouteTest';
+
+import Home from './pages/Home';
+import Edit from './pages/Edit';
+import Diary from './pages/Diary';
+import New from './pages/New';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h2>dks</h2>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/new' element={<New />} />
+          <Route path='/edit' element={<Edit />} />
+          <Route path='/diary' element={<Diary />} />
+        </Routes>
+        <RouteTest />
+
+      </div>
+    </BrowserRouter>
   );
 }
 
