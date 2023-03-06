070190fb17f524cadc6d54a45d6e42564be64102
MinJoon_Choi <minjoon1998@gmail.com>
2023-03-07 03:36:50 +0900

    EmotionDiary-new page

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,21 +1,9 @@
-import { useNavigate } from "react-router-dom";
-
-import MyButton from "../components/MyButton";
-import MyHeader from "../components/MyHeader";
+import DiaryEditor from "../components/DiaryEditor";
 
 const New = () => {
-    const navigate = useNavigate();
     return (
         <div>
-            <MyHeader
-                headText={"새 일기쓰기"}
-                leftChild={<MyButton text={"< 뒤로가기"} onClick={() => navigate(-1)} />}
-            />
-            <div>
-                <section>
-                    <h4>오늘은 언제인가요?</h4>
-                </section>
-            </div>
+            <DiaryEditor />
         </div>
     );
 };
